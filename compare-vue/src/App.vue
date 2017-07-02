68891d4d5934533acad8ef177f305b5671fbbff0
Cesar Frick <cesarfrick@gmail.com>
2017-07-02 01:53:22 -0500

    [App] Start with the footer
    Lesson 15

--- compare-vue/src/App.vue
+++ compare-vue/src/App.vue
@@ -35,6 +35,20 @@
       </div>
     </div>
     <router-view></router-view>
+
+    <footer class="footer is-primary">
+      <div class="container">
+        <div class="columns">
+          <div class="column">
+            <p>And this right here is a spiffy footer, where you can put stuff.</p>
+          </div>
+          <div class="column has-text-right">
+            <a class="icon" href="#"><i class="fa fa-facebook"></i></a>
+            <a class="icon" href="#"><i class="fa fa-twitter"></i></a>
+          </div>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
@@ -83,6 +97,15 @@ export default {
     span {
       background-color: #c1c1c1;
     }
+  }
+}
+
+footer {
+  background-color: $primary !important;
+
+  .icon {
+    color: #fff;
+    margin-left: 20px;
   }
 }
 

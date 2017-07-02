dd04101c65d4eddb33dc6f19d4e920c2207f1656
Cesar Frick <cesarfrick@gmail.com>
2017-07-02 03:30:21 -0500

    [FAQ] Create FAQ cards and implement an external data endpoint
    Lesson 18

--- compare-vue/src/components/Faq.vue
+++ compare-vue/src/components/Faq.vue
@@ -1,32 +1,61 @@
 <template>
   <div class="faq">
-      FAQ
+      <div class="container">
+        <section class="section">
+          <h1 class="title">FAQ</h1>
+          <h2 class="subtitle is-4">Lorem ipsum and all that jazz</h2>
+
+          <div class="columns" v-if="faqs && faqs.length">
+            <div class="column is-one-third" v-for="faq of faqs" :key="faq.id">
+              <div class="card">
+                <div class="card-content">
+                  <p class="title">{{ faq.title }}</p>
+                  <p class="subtitle">{{ faq.body }}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'faq',
+  data: () => ({
+    faqs: [],
+    errors: [],
+  }),
+
+  created() {
+    axios.get('http://jsonplaceholder.typicode.com/posts')
+      .then((response) => {
+        this.faqs = response.data.slice(0, 10);
+      })
+      .catch((e) => {
+        this.errors.push(e);
+      });
+  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
+<style lang="scss" scoped>
+@import '../main.scss';
+
+.pd {
+  padding: 2.5em 0 1.5em;
 }
 
-ul {
-  list-style-type: none;
-  padding: 0;
+.answer {
+  color: gray;
+  margin-top: 10px !important;
 }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+.columns {
+  flex-wrap: wrap;
 }
 </style>

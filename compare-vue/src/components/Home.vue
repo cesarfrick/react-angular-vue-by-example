27358d0022de191d01eae37ebdfff938b9b39c20
Cesar Frick <cesarfrick@gmail.com>
2017-07-02 02:42:54 -0500

    [Home] Create the Hero section
    Lesson 16

--- compare-vue/src/components/Home.vue
+++ compare-vue/src/components/Home.vue
@@ -1,32 +1,56 @@
 <template>
   <div class="home">
-    Home
+    <section class="hero">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">{{ heading }}</h1>
+          <div class="is-two-thirds column is-paddingless">
+            <h2 class="subtitle is-4">{{ subheading }}</h2>
+          </div>
+          <a class="button is-large is-primary" id="learn">Learn more</a>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
 export default {
   name: 'home',
+  data() {
+    return {
+      heading: 'Soaring to new heights',
+      subheading: 'Et aliqua reprehenderit dolore laboris enim id cupidatat ad sit do sint proident reprehenderit occaecat. Nulla ipsum ex veniam esse.',
+    };
+  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
+<style lang="scss" scoped>
+@import "../main.scss";
+
+.hero {
+  background: url('../assets/clouds.jpg');
+  background-size: cover;
+
+  .title {
+    @include mobile {
+      font-weight: bold;
+    }
+
+    @include tablet {
+      font-size: 2.5rem;
+    }
+
+    @include desktop {
+      font-size: 4rem;
+      margin-top: 2rem;
+    }
+  }
 }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+h2 {
+  margin: 1.5rem 0 2rem !important;
 }
 </style>

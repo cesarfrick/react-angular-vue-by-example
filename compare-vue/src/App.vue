1f85712df6980f4724bbd089bd090de5884969b5
Cesar Frick <cesarfrick@gmail.com>
2017-07-02 06:53:17 -0500

    [Header] Move the header to its own component

--- compare-vue/src/App.vue
+++ compare-vue/src/App.vue
@@ -1,39 +1,6 @@
 <template>
   <div id="app">
-    <div class="nav has-shadow">
-      <div class="container">
-        <div class="nav-left">
-          <a class="nav-item">My Company</a>
-        </div>
-
-        <span :class="['nav-toggle', { 'is-active': isActive }]"
-              @click="toggleNav"
-        >
-          <span></span>
-          <span></span>
-          <span></span>
-        </span>
-
-        <div :class="['nav-right', 'nav-menu', { 'is-active': isActive }]">
-          <router-link to="/" class="nav-item r-item">Home</router-link>
-          <router-link to="/faq" class="nav-item r-item">Features</router-link>
-          <router-link to="/faq" class="nav-item r-item">About</router-link>
-          <router-link to="/faq" class="nav-item r-item">Faq</router-link>
-
-          <div class="nav-item">
-            <p class="control">
-              <a class="button is-primary is-outlined">
-                <span class="icon">
-                  <i class="fa fa-download"></i>
-                </span>
-
-                <span>Join Now</span>
-              </a>
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
+    <v-header></v-header>
     <router-view></router-view>
 
     <footer class="footer is-primary">
@@ -53,52 +20,18 @@
 </template>
 
 <script>
+import Header from './components/Header';
+
 export default {
   name: 'app',
-  data() {
-    return { isActive: false };
-  },
-  methods: {
-    toggleNav() {
-      this.isActive = !this.isActive;
-    },
+  components: {
+    'v-header': Header,
   },
 };
 </script>
 
 <style lang="scss">
 @import "main.scss";
-
-.nav {
-  background-color: #383838;
-
-  a:hover {
-    color: gray;
-  }
-
-  &-left a{
-    color: #fff;
-    font-weight: bold;
-  }
-
-  .r-item {
-    color: #c1c1c1;
-    padding: .5rem 1.75rem;
-
-    @include mobile {
-      color: gray;
-      &:hover {
-        background-color: #f1f1f1;
-      }
-    }
-  }
-
-  &-toggle {
-    span {
-      background-color: #c1c1c1;
-    }
-  }
-}
 
 footer {
   background-color: $primary !important;
@@ -108,5 +41,4 @@ footer {
     margin-left: 20px;
   }
 }
-
 </style>
---
+++ compare-vue/src/components/Header.vue
@@ -0,0 +1,90 @@
+<template>
+  <header class="nav has-shadow">
+      <div class="container">
+        <div class="nav-left">
+          <router-link to="/" class="nav-item">My Company</router-link>
+        </div>
+
+        <span :class="['nav-toggle', { 'is-active': isActive }]"
+              @click="toggleNav"
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </span>
+
+        <div :class="['nav-right', 'nav-menu', { 'is-active': isActive }]">
+          <nav v-for="(link, index) in routeLinks" :key="index">
+            <router-link :to="link.route" class="nav-item r-item">{{ link.text }}</router-link>
+          </nav>
+
+          <div class="nav-item">
+            <p class="control">
+              <a class="button is-primary is-outlined">
+                <span class="icon">
+                  <i class="fa fa-download"></i>
+                </span>
+
+                <span>Join Now</span>
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </header>
+</template>
+
+<script>
+export default {
+  name: 'header',
+  data: () => ({
+    routeLinks: [
+      { route: '/', text: 'Home' },
+      { route: '/faq', text: 'Features' },
+      { route: '/faq', text: 'About' },
+      { route: '/faq', text: 'Faq' },
+    ],
+    isActive: false,
+  }),
+  methods: {
+    toggleNav() {
+      this.isActive = !this.isActive;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../main.scss';
+
+.nav {
+  background-color: #3b3b3b;
+
+  a:hover {
+    color: gray;
+  }
+
+  &-left a{
+    color: #fff;
+    font-weight: bold;
+  }
+
+  .r-item {
+    color: #c1c1c1;
+    padding: .5rem 1.75rem;
+
+    @include mobile {
+      color: gray;
+      &:hover {
+        background-color: #f1f1f1;
+      }
+    }
+  }
+
+  &-toggle {
+    span {
+      background-color: #c1c1c1;
+    }
+  }
+}
+</style>

c4ca1173979b31890b934bd7e9ad7fa1624e1f82
Cesar Frick <cesarfrick@gmail.com>
2017-07-02 10:34:15 -0500

    Move Hero to its own component

---
+++ compare-vue/src/components/Hero.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class="hero">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">{{ this.heading }}</h1>
+        <div class="is-two-thirds column is-paddingless">
+          <h2 class="subtitle is-4">{{ this.subheading }}</h2>
+        </div>
+          <a class="button is-large is-primary" id="learn">Learn more</a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'hero',
+  props: ['heading', 'subheading'],
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../main.scss';
+
+.hero {
+  background: url('../assets/clouds.jpg');
+  background-size: cover;
+
+  .title {
+    @include mobile {
+      font-weight: bold;
+    }
+
+    @include tablet {
+      font-size: 2.5rem;
+    }
+
+    @include desktop {
+      font-size: 4rem;
+      margin-top: 2rem;
+    }
+  }
+}
+</style>

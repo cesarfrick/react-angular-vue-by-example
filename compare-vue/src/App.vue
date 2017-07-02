66338b4420728df56209280f64009e3efd464477
Cesar Frick <cesarfrick@gmail.com>
2017-07-02 07:03:30 -0500

    [Footer] Move footer to its own component

--- compare-vue/src/App.vue
+++ compare-vue/src/App.vue
@@ -1,44 +1,22 @@
 <template>
   <div id="app">
     <v-header></v-header>
+
     <router-view></router-view>
 
-    <footer class="footer is-primary">
-      <div class="container">
-        <div class="columns">
-          <div class="column">
-            <p>And this right here is a spiffy footer, where you can put stuff.</p>
-          </div>
-          <div class="column has-text-right">
-            <a class="icon" href="#"><i class="fa fa-facebook"></i></a>
-            <a class="icon" href="#"><i class="fa fa-twitter"></i></a>
-          </div>
-        </div>
-      </div>
-    </footer>
+    <v-footer></v-footer>
   </div>
 </template>
 
 <script>
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 export default {
   name: 'app',
   components: {
     'v-header': Header,
+    'v-footer': Footer,
   },
 };
 </script>
-
-<style lang="scss">
-@import "main.scss";
-
-footer {
-  background-color: $primary !important;
-
-  .icon {
-    color: #fff;
-    margin-left: 20px;
-  }
-}
-</style>
---
+++ compare-vue/src/components/Footer.vue
@@ -0,0 +1,34 @@
+<template>
+  <footer class="footer is-primary">
+      <div class="container">
+        <div class="columns">
+          <div class="column">
+            <p>And this right here is a spiffy footer, where you can put stuff.</p>
+          </div>
+          <div class="column has-text-right">
+            <a class="icon" href="#"><i class="fa fa-facebook"></i></a>
+            <a class="icon" href="#"><i class="fa fa-twitter"></i></a>
+          </div>
+        </div>
+      </div>
+    </footer>
+</template>
+
+<script>
+export default {
+  name: 'footer',
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../main.scss";
+
+.footer {
+  background-color: $primary;
+
+  .icon {
+    color: #fff;
+    margin-left: 20px;
+  }
+}
+</style>

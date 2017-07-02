32c591d0788b9003db9d074a2a9cd6443e4519bf
Cesar Frick <cesarfrick@gmail.com>
2017-07-02 01:16:59 -0500

    First commit for Vue exercise
    Lessons 1 to 13 in this commit

---
+++ compare-vue/src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="app">
+    <div class="nav has-shadow">
+      <div class="container">
+        <div class="nav-left">
+          <a class="nav-item">My Company</a>
+        </div>
+
+        <span class="nav-toggle">
+          <span></span>
+          <span></span>
+          <span></span>
+        </span>
+
+        <div class="nav-right nav-menu">
+          <router-link to="/" class="nav-item r-item">Home</router-link>
+          <router-link to="/faq" class="nav-item r-item">Features</router-link>
+          <router-link to="/faq" class="nav-item r-item">About</router-link>
+          <router-link to="/faq" class="nav-item r-item">Faq</router-link>
+
+          <div class="nav-item">
+            <p class="control">
+              <a class="button is-primary is-outlined">
+                <span class="icon">
+                  <i class="fa fa-download"></i>
+                </span>
+
+                <span>Join Now</span>
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+};
+</script>
+
+<style lang="scss">
+@import "main.scss";
+
+.nav {
+  background-color: #383838;
+
+  a:hover {
+    color: gray;
+  }
+
+  &-left a{
+    color: #fff;
+    font-weight: bold;
+  }
+
+  .r-item {
+    color: #c1c1c1;
+    padding: .5rem 1.75rem;
+
+    @include mobile {
+      color: gray;
+      &:hover {
+        background-color: #f1f1f1;
+      }
+    }
+  }
+}
+
+</style>
